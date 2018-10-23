0fa8f121f214cae1fd3928bb8cbfa902cc44c609
Jimmy <jimmy@tawfik.nl>
2018-10-23 15:11:09 +0200

    serverTest

---
+++ public/javascript/javascript_group.js
@@ -0,0 +1,15 @@
+/*
+JavaScript for Group Project Business Page.
+By: Jimmy Tawfik and Michael Wood
+Business: School Website 
+*/
+
+/* Open when someone clicks on the span element */
+function openNav() {
+    document.getElementById("myNav").style.width = "15%";
+}
+
+/* Close when someone clicks on the "x" symbol inside the overlay */
+function closeNav() {
+    document.getElementById("myNav").style.width = "0%";
+} 
